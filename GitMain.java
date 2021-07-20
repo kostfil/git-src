b86474a25638c11237b5551f0a229ea4398aac3b
kostfil <kostfil@yandex.ru>
2021-07-20 17:00:14 +0300

    second commit

--- GitMain.java
+++ GitMain.java
@@ -2,6 +2,5 @@ public class GitMain {
     public static void main(String[] args) {
         System.out.println("Hello, Git");
         System.out.println("Hello, Git 2");
-        System.out.println("Hello, Git 3");
     }
 }
